c8d2a9339c1c98b8465f65e68161f653a02dc028
marcolaleal <marcolaleal@gmail.com>
2023-03-15 07:24:52 -0300

    feat: add bcrypt in project

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,28 +2,32 @@ import userService from "../services/user.service.js";
 
 
 const create = async (req,res) => {
-    const {name, username, email, password, avatar, background} = req.body;
-
-    if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).json({message:"Submit all field for registration"});
+    try {
+        const {name, username, email, password, avatar, background} = req.body;
+    
+        if (!name || !username || !email || !password || !avatar || !background) {
+            res.status(400).json({message:"Submit all field for registration"});
+        }
+    
+        const user = await userService.create(req.body);
+    
+        if (!user){
+            res.status(400).json({message:"Error creating User"});
+        }
+    
+        res.status(201).send({
+            message: "User created successfuly",
+            user: {
+                id: user._id,
+                name,
+                username,
+                email,
+                avatar,
+                background
+        }});    
+    } catch (error) {
+        res.status(500).send({message: error.message});
     }
-
-    const user = await userService.create(req.body);
-
-    if (!user){
-        res.status(400).json({message:"Error creating User"});
-    }
-
-    res.status(201).send({
-        message: "User created successfuly",
-        user: {
-            id: user._id,
-            name,
-            username,
-            email,
-            avatar,
-            background
-    }});
 };
 
 const findAll = async (req,res) => {
